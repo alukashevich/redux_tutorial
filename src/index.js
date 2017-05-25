43ddf3266eeda337bac31c93b0805ef28b08f980
alukashevich <Administrator@ALUKASHEVICH-VM.camstar.local>
2017-05-25 12:38:35 +0300

    add reducer

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import 'babel-polyfill'
 import React from 'react'
 import { render } from 'react-dom'
-import { createStore } from 'redux'
 import { Provider } from 'react-redux'
 import App from './containers/App'
 import configureStore from './store/configureStore'
