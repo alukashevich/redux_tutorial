0fb5b8a01d9d114cf23003c60576981875c8d1b9
alukashevich <Administrator@ALUKASHEVICH-VM.camstar.local>
2017-05-25 15:25:38 +0300

    Presentational components fix bug

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import User from '../components/User'
+import Page from '../components/Page'
 
 class App extends Component {
 
