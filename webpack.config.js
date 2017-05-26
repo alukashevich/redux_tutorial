d9d28db6b337661a096e097b463b63800138e147
alukashevich <alex_lukashevich@tut.by>
2017-05-26 16:17:57 +0000

    change webpack

--- webpack.config.js
+++ webpack.config.js
@@ -11,9 +11,9 @@ module.exports = {
     './src/index'
   ],
   output: {
-    path: path.join(__dirname, 'dist'),
+    //path: path.join(__dirname, 'dist'),
     filename: 'bundle.js',
-    publicPath: '/static/'
+    //publicPath: '/static/'
   },
   plugins: [
     new webpack.optimize.OccurenceOrderPlugin(),
